965e56f53ecfab573041b541e8a7863ed765c396
Jason Kao <jasonkao85@gmail.com>
2017-12-05 20:58:49 -0500

    gitignore rm cached

--- elpa/0blayout-1.0.2/0blayout-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "0blayout" "1.0.2" "Layout grouping with ease" 'nil :commit "b8bb44b5336965b5519b9a826a0f46f8ee31c471" :url "https://github.com/etu/0blayout" :keywords '("convenience" "window-management"))
