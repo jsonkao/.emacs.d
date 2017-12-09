d9bacda1281bf97ad3b568d1f300645eb4cc19f9
Jason Kao <jasonkao85@gmail.com>
2017-12-09 01:14:15 -0500

    fullscreen on start

--- init.el
+++ init.el
@@ -32,3 +32,8 @@
 	(move-region start end (if (null n) 1 n)))
 
 (setq inhibit-startup-screen t)
+(setq global-linum-mode t)
+
+(custom-set-variables
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
