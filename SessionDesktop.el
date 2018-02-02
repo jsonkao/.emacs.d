0f8362e55d445a34e415176b0ba5475759ef1b9d
Jason Kao <jasonkao85@gmail.com>
2018-02-01 19:30:31 -0500

    web-mode

---
+++ SessionDesktop.el
@@ -0,0 +1,47 @@
+;; -*- mode: emacs-lisp; coding: emacs-mule; -*-
+;; --------------------------------------------------------------------------
+;; Desktop File for Emacs
+;; --------------------------------------------------------------------------
+;; Created Thu Jan 25 09:10:15 2018
+;; Desktop file format version 208
+;; Emacs version 25.1.1
+(setq revive:frame-configuration-to-restore
+  '(progn 
+     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (visibility . t) (icon-name) (bottom-divider-width . 0) (right-divider-width . 0) (top . 30) (left . 0) (unsplittable) (modeline . t) (width . 75) (height . 30) (cursor-color . "Red") (background-mode . light) (fringe) (fullscreen) (alpha) (scroll-bar-height . 0) (scroll-bar-width . 15) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 8) (left-fringe . 1) (line-spacing) (background-color . "White") (foreground-color . "Black") (horizontal-scroll-bars) (vertical-scroll-bars . right) (internal-border-width . 0) (border-width . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(75 30 ((0 0 78 29)) ((nil "*Messages*" 1245 1137 nil))))
+     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (bottom-divider-width . 0) (right-divider-width . 0) (top . 83) (left . 386) (unsplittable) (modeline . t) (width . 81) (height . 35) (fringe) (mouse-color . "black") (environment) (visibility . t) (cursor-color . "Red") (background-mode . light) (fullscreen) (alpha) (scroll-bar-height . 0) (scroll-bar-width . 15) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 8) (left-fringe . 1) (line-spacing) (background-color . "White") (foreground-color . "Black") (horizontal-scroll-bars) (vertical-scroll-bars . right) (internal-border-width . 0) (border-width . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(81 35 ((0 0 84 34)) (("~/Documents/test/Untitled.tex" "Untitled.tex" 1009 676 ((tab "Untitled.tex" "/Users/jasonkao/Documents/test/Untitled.tex"))))))))
+
+;; Global section:
+(setq desktop-saved-frameset [frameset 1 (23145 58695 970840 0) (desktop . "208") "jasonkao@Jasons-MBP.fios-router.home" nil nil ((((font-backend mac-ct) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (horizontal-scroll-bars) (foreground-color . "Black") (background-color . "White") (line-spacing) (left-fringe . 1) (right-fringe . 8) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (scroll-bar-height . 0) (alpha) (fullscreen) (fringe) (display-type . color) (background-mode . light) (cursor-color . "Red") (frameset--id . "A5D7-8BA3-FD69-4A5B") (frameset--mini t) (height . 30) (width . 75) (modeline . t) (minibuffer . t) (unsplittable) (left . 0) (top . 30) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (visibility . t) (display . "Jasons-MBP.fios-router.home") (explicit-name) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 2) (min-width-ignore . 9) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 64) (min-pixel-width . 70) (min-pixel-height-ignore . 32) (min-pixel-width-ignore . 59) (min-pixel-height-safe . 16) (min-pixel-width-safe . 14)) leaf (pixel-width . 549) (pixel-height . 464) (total-width . 78) (total-height . 29) (normal-height . 1.0) (normal-width . 1.0) (buffer "*Messages*" (selected . t) (hscroll . 0) (fringes 1 8 nil) (margins 3) (scroll-bars nil 3 t nil 0 nil) (vscroll . 0) (dedicated . t) (point . 1245) (start . 1137))) (((font-backend mac-ct) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (horizontal-scroll-bars) (foreground-color . "Black") (background-color . "White") (line-spacing) (left-fringe . 1) (right-fringe . 8) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (scroll-bar-height . 0) (alpha) (fullscreen) (display-type . color) (background-mode . light) (cursor-color . "Red") (visibility . t) (environment) (mouse-color . "black") (fringe) (frameset--id . "3F31-BC63-A059-7234") (frameset--mini t . t) (height . 35) (width . 81) (modeline . t) (minibuffer . t) (unsplittable) (left . 386) (top . 83) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "Jasons-MBP.fios-router.home") (explicit-name) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 4) (min-width-ignore . 9) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 64) (min-pixel-width . 70) (min-pixel-height-ignore . 51) (min-pixel-width-ignore . 59) (min-pixel-height-safe . 16) (min-pixel-width-safe . 14)) leaf (pixel-width . 591) (pixel-height . 544) (total-width . 84) (total-height . 34) (normal-height . 1.0) (normal-width . 1.0) (buffer "Untitled.tex" (selected) (hscroll . 0) (fringes 1 8 nil) (margins 3) (scroll-bars nil 3 t nil 0 nil) (vscroll . 0) (dedicated) (point . 1009) (start . 676))))])
+(setq desktop-missing-file-warning nil)
+(setq tags-file-name nil)
+(setq tags-table-list nil)
+(setq search-ring '("tex"))
+(setq regexp-search-ring nil)
+(setq register-alist nil)
+(setq file-name-history '("~/Library/Preferences/Aquamacs Emacs/customizations.el" "~/Documents/test/Untitled.tex"))
+
+;; Buffer section -- buffers listed in same order as in buffer list:
+(desktop-create-buffer 208
+  "/Users/jasonkao/Documents/test/Untitled.tex"
+  "Untitled.tex"
+  'latex-mode
+  '(auto-fill-mode smart-spacing-mode linum-mode TeX-PDF-mode bib-cite-minor-mode reftex-mode)
+  1009
+  '(nil nil)
+  nil
+  nil
+  '((indent-tabs-mode) (buffer-file-coding-system . utf-8-unix) (case-fold-search . t) (truncate-lines))
+  '((mark-ring nil)))
+
+(desktop-create-buffer 208
+  "/Users/jasonkao/Library/Application Support/Aquamacs Emacs/scratch buffer"
+  "*scratch*"
+  'text-mode
+  '(auto-fill-mode smart-spacing-mode linum-mode TeX-PDF-mode)
+  1
+  '(nil nil)
+  nil
+  nil
+  '((buffer-file-coding-system . undecided-unix) (truncate-lines))
+  '((mark-ring nil)))
+
