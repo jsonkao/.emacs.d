f5ed1ab0be45201aa7585cfe02ee20f53ecb4fa0
Jason Kao <jason.kao@columbia.edu>
2021-11-26 15:36:13 -0500

    elpa

---
+++ elpa/reformatter-20200814.435/reformatter-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jasonkao/.emacs.d/elpa/reformatter-20200814.435/reformatter.el  -*- no-byte-compile: t -*-
+(define-package "reformatter" "20200814.435" "Define commands which run reformatters on the current buffer" '((emacs "24.3")) :commit "5aa8c1867950b9b2ba6e583271f9c0d715853c5a" :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :keywords '("convenience" "tools") :url "https://github.com/purcell/reformatter.el")
